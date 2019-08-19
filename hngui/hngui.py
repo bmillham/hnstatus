2a7b21c48b7d4a514170e86cd06d9920e1287a15
Brian Millham <bmillham@gmail.com>
2019-08-18 23:42:56 -0400

    Revert "Stop forcing text color to black on the progress bar "
    This reverts commit bb9f05187f3dc7e6282d9a530a125e6cf65f297e.

--- hngui/hngui.py
+++ hngui/hngui.py
@@ -91,7 +91,8 @@ class HnGui():
         self.background_color = background_color
         self.highlight_color = Gdk.RGBA()
         self.highlight_color.parse('red')
-        self.normal_color = None  # Leave the color alone if not highlighted
+        self.normal_color = Gdk.RGBA()
+        self.normal_color.parse('black')
 
         # Setup the appindicator
         self.downicon = os.path.join(path,
