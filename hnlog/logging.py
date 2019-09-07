0269fce7b563065d3a981657c9af4a55eb32608d
Brian Millham <bmillham@gmail.com>
2019-09-07 03:33:45 -0400

    Got basic stats inserted to the database working.

--- hnlog/logging.py
+++ hnlog/logging.py
@@ -1,6 +1,7 @@
 """ Log usage and status information to a sqlite3 database """
 
 import sqlite3
+from datetime import datetime
 #from sqlite import Error
 
 
@@ -13,6 +14,26 @@ class Log(object):
     def open(self):
         self.connection = sqlite3.connect(self.database)
         self.cursor = self.connection.cursor()
+        # Create the table if new database
+        s = """CREATE TABLE IF NOT EXISTS
+               activity(adate DATETIME PRIMARY KEY,
+               rx REAL, tx REAL, ss INTEGER);"""
+        self._execute(s)
 
-    def _create_table(self):
-        pass
+    def _execute(self, sql, params=None, commit=True):
+        if params:
+            self.cursor.execute(sql, params)
+        else:
+            self.cursor.execute(sql)
+        if commit:
+            self.connection.commit()
+
+    def close(self):
+        self.connection.close()
+
+    def adddata(self, tx, rx, ss):
+        now = datetime.now()
+        s = """INSERT INTO activity(adate, rx, tx, ss)
+               VALUES(?, ?, ?, ?);"""
+        self._execute(s, params=(now, rx, tx, ss))
+    
